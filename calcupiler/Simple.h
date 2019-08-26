dc71ce494e3e4b4cec4ed2b17154e68d1a1c4af9
Ibrahim T <blablablub@gmail.com>
2019-08-26 20:18:17 +0200

    initial commit

---
+++ calcupiler/Simple.h
@@ -0,0 +1,165 @@
+#pragma once
+
+#include "Calcupiler.h"
+#include <limits>
+#include <type_traits>
+#include <typeinfo>
+//#include <cmath>
+
+namespace Calcupiler
+{
+	namespace Simple
+	{
+		template<typename T, typename = std::remove_reference_t<std::decay_t<T>>>
+		class CSimple
+		{
+		public:
+			//constexpr bool is_type_char(){
+			//	return std::is_same_v<T, char>;
+			//};
+			static_assert(
+				(std::is_integral_v<T> || std::is_floating_point_v<T>), 
+				"Type must be either integral or floating point"
+				);
+			static_assert(!std::is_same_v<T, char>, "Character type is not allowed");
+
+			constexpr CSimple() = delete;
+			~CSimple() = delete;
+
+			//static constexpr SimpleCalc<T>& operator+(const SimpleCalc<T>& rhs) {
+			//	return this;
+			//}
+
+			//######################################################
+			static constexpr T Sum(const T lhs, const T rhs) {
+				return lhs + rhs;
+			}
+
+			static constexpr T Diff(const T lhs, const T rhs) {
+				return lhs - rhs;
+			}
+
+			static constexpr T Mul(const T lhs, const T rhs) {
+				return lhs * rhs;
+			}
+
+			static constexpr T Div(const T lhs, const T rhs) {
+				return lhs / rhs;
+			}
+
+			//#########################################################
+
+			/// <summary>
+			/// Power of exp over basis
+			/// </summary>
+			template<typename E>
+			static constexpr T Pow(T basis, E exp) {
+				return std::is_integral_v<E> ? 
+					Pow_i(basis, exp) : Pow_f(basis, exp);
+			}
+
+			/// <summary>
+			/// Power for integral type of exp over basis
+			/// </summary>
+			template<typename E, typename = std::is_integral<E>>
+			static constexpr T Pow_i(T basis, E exp) {
+				return exp == 1 ? basis : basis * Pow_i(basis, exp - 1);
+			}
+
+			/// <summary>
+			/// Power for floaing point of exp over basis
+			/// </summary>
+			template<typename E, typename = std::is_floating_point<E>>
+			static constexpr T Pow_f(T basis, E exp) {
+				return exp == 1 ? basis : basis * Pow_f(basis, exp - 1);
+			}
+
+			/// <summary>
+			/// The factorial of k
+			/// </summary>
+			static constexpr T factorial(T k) {
+				return std::is_integral_v<T> ? 
+					 factorial_i(k) : factorial_f(k);
+			}
+
+			template<typename E, typename = std::is_integral<E>>
+			static constexpr E factorial_i(E k) {
+				static_assert(std::is_integral_v<E>, "Only integral types are allowed.");
+				return k < 2 ? k : k * factorial_i(--k);
+				//TODO:: implement floating point factorial
+			}
+
+			static constexpr T factorial_f(T k) {
+				return 1.0;
+				//TODO:: implement floating point factorial
+			}
+			
+			//static constexpr T bla = factorial(3);
+			/// <summary>
+			/// The euler's number.
+			/// </summary>
+			/// <param name="k">defines the calculated precision of the euler's number. Minimum value is 10 and maximum value is 100</param>
+			template<
+				typename E, typename = std::is_integral<E>
+			> 
+			static constexpr T e(E k) {
+				//static_assert(std::is_floating_point_v<E>, "Only floating point types are allowed.")
+				T res = 2.0;
+				while (k > 1) 
+				{
+					res += 1.0 / factorial_i(k--);
+				}
+				return static_cast<T>(res);
+			}
+			//static constexpr T e(K k) {
+			//	return k;
+			//}
+
+			#define e e(10)
+
+			///// <summary>
+			///// Power of exp over e (euler's number)
+			///// </summary>
+			template<typename E, typename = std::is_integral<E>>
+			static constexpr T e_exp(E exp) {
+				return std::is_integral_v<E> ? 
+					(exp == 1 ? e : Pow_i(e, exp)) : e_exp_f(exp, 1.0, 10);
+			}
+
+			//template<typename E, typename = std::is_integral<E>>
+			//static constexpr T e_exp_i(T exp, T res, E n) {
+			//	return exp == 1.0 ? e :
+			//		n == 0 ? res : e_exp_i(exp, res + (Pow(exp, n) / factorial(n), --n));
+			//}
+
+			template<typename E, typename = std::is_integral<E>>
+			static constexpr T e_exp_f(T exp, T res, E n) {
+				return exp == 1.0 ? e : 
+					n == 0 ? res : e_exp_f(exp, res + (Pow_i(exp, n) / factorial_i(n)), n-1);
+			}
+			//static constexpr T series()
+			
+			/// <summary>
+			/// Logarithmus naturales of x
+			/// </summary>
+			static constexpr T ln(T x)  {
+				if (x == static_cast<T>(1)) return 0;
+
+				T res = 0.0;
+				constexpr auto minus = static_cast<T>(-1);
+				constexpr auto one = static_cast<T>(1);
+				for (int k = 1; k < 100; k++)
+				{
+					res += ((Pow(minus, (k + 1)) / k) * Pow(x, k));
+				}
+
+				return e_exp(res);
+			}
+
+		private:
+
+			//constexpr T m_value = 2.0;
+
+		};
+	}
+}
---
+++ calcupiler/Source.cpp
@@ -0,0 +1,60 @@
+#include "Simple.h"
+#include <string>
+#include <math.h>
+
+using namespace Calcupiler::Simple;
+
+using SimpleCalculator = CSimple<double>;
+
+constexpr double tolerance = 0.00001;
+
+constexpr bool isWithinTolerance(double givenVal, double expectedValue) {
+	return givenVal > expectedValue - tolerance && givenVal < expectedValue + tolerance;
+}
+
+int main() 
+{
+
+	constexpr auto sum = CSimple<double>::Sum(1.3, 4.2);
+	static_assert(isWithinTolerance(sum, 5.5), "mul function failed");
+
+	constexpr auto diff = CSimple<double>::Diff(3.45, 5.2);
+	static_assert(isWithinTolerance(diff, -1.75), "diff function failed");
+
+	constexpr auto mul = CSimple<double>::Mul(1.3, 4.2);
+	static_assert(isWithinTolerance(mul, 5.46), "mul function failed");
+
+	constexpr auto div = CSimple<double>::Div(1.45, 2.2);
+	static_assert(isWithinTolerance(div, 0.6591), "div function failed");
+
+	constexpr auto pow1 = CSimple<double>::Pow(1.4, 2);
+	static_assert(isWithinTolerance(pow1, 1.96), "pow function failed");
+	//constexpr auto pow2 = CSimple<double>::Pow(1.4, 0.24);
+	//static_assert(isWithinTolerance(pow2, 0.0841), "pow function failed");
+
+	constexpr auto factorial1 = CSimple<int>::factorial(1);
+	static_assert(isWithinTolerance(factorial1, 1), "factorial function failed");
+	constexpr auto factorial2 = CSimple<int>::factorial(5);
+	static_assert(isWithinTolerance(factorial2, 120), "factorial function failed");
+	//constexpr auto factorial_f1 = CSimple<int>::factorial(0.45);
+	//static_assert(isWithinTolerance(factorial_f1, 120), "factorial function failed");
+
+	constexpr auto euler = CSimple<double>::e;
+	static_assert(isWithinTolerance(euler, 2.71828), "euler function failed");
+
+	constexpr auto e_y1 = CSimple<double>::e_exp(3);
+	static_assert(isWithinTolerance(e_y1, 20.08553), "euler function failed");
+	constexpr auto e_y2 = CSimple<double>::e_exp(0.5);
+	static_assert(isWithinTolerance(e_y2, 1.64872), "euler function failed");
+	constexpr auto e_y = CSimple<double>::e_exp(-0.4);
+	static_assert(isWithinTolerance(e_y, 0.67032), "euler function failed");
+
+	//constexpr auto ln = CSimple<double>::ln(0.5);
+
+	//constexpr auto ln2 = CSimple<int>::ln('a');
+
+	//static_assert(ln > 20, "bla");
+	return 0;
+}
+
+//constexpr auto bla = ceil(10);
